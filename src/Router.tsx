7bdfae6614d47d76e83477365c2b3ff0266ab0d2
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-11-19 19:36:23 +0100

    feat(theming): add dark/ligh theme toggle

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-  Login
+  Login,
+  NotFound
 } from "./pages";
 
 function Router() {
@@ -8,6 +9,7 @@ function Router() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
   )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import { UserAuthForm } from "@/components/ui/user-auth-form"
 import { Link } from "react-router-dom"
 import { useState } from "react"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 function Login() {
   const [mode, setMode] = useState<"login" | "register">("login")
@@ -9,13 +10,15 @@ function Login() {
   return (
     <>
       <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Button
-          variant="ghost"
-          onClick={() => setMode(mode === "login" ? "register" : "login")}
-          className="absolute right-4 top-4 md:right-8 md:top-8"
-        >
-          {mode === "login" ? "Register" : "Login"}
-        </Button>
+        <div className="absolute right-8 top-8 md:right-16 md:top-16 flex items-center space-x-4">
+          <Button
+            variant="ghost"
+            onClick={() => setMode(mode === "login" ? "register" : "login")}
+          >
+            {mode === "login" ? "Register" : "Login"}
+          </Button>
+          <ThemeToggle />
+        </div>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
