bf80dcd114b790306c43cb93e4031d3320d763fa
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2024-12-09 02:02:35 +0100

    feat(playground deletion): confirmation modal

--- src/pages/Playground.tsx
+++ src/pages/Playground.tsx
@@ -8,12 +8,23 @@ import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 function Playground() {
   const { id } = useParams<{ id: string }>();
   const [playground, setPlayground] = useState<any>(null);
   const { backendAddress, token } = useAuth();
   const navigate = useNavigate();
   const { toast } = useToast();
+
+  const [isDeletionDialogOpen, setIsDeletionDialogOpen] = useState(false);
 
   useEffect(() => {
     if (!id)
@@ -24,7 +35,7 @@ function Playground() {
   }, [backendAddress, token]);
 
   return (
-    <div>
+    <>
       <div className="flex justify-between items-center mb-4">
         <Input
           value={playground?.name}
@@ -34,16 +45,7 @@ function Playground() {
         />
         <div>
           <Button
-            onClick={() => {
-              if (!id) return;
-              deletePlayground(backendAddress, token as string, parseInt(id))
-                .then(() => {
-                  navigate("/");
-                })
-                .catch((err) => {
-                  toast({ title: "Error", description: err.message });
-                });
-            }}
+            onClick={() => setIsDeletionDialogOpen(true)}
             variant={"destructive"}
           >
             <Trash size={24} />
@@ -51,8 +53,38 @@ function Playground() {
           </Button>
         </div>
       </div>
+
+      <Dialog open={isDeletionDialogOpen} onOpenChange={setIsDeletionDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Playground</DialogTitle>
+            <DialogDescription>Are you sure you want to delete the playground?</DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button variant="default" onClick={() => setIsDeletionDialogOpen(false)}>
+              Cancel
+            </Button>
+            <Button
+              variant="destructive"
+              onClick={() => {
+                if (!id) return;
+                deletePlayground(backendAddress, token as string, parseInt(id))
+                  .then(() => {
+                    navigate("/");
+                  })
+                  .catch((err) => {
+                    toast({ title: "Error", description: err.message });
+                  });
+              }}
+            >
+              Delete
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
       <Canva playground={playground} setPlayground={setPlayground} />
-    </div>
+    </>
   );
 }
 
