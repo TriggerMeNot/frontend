ad4110c64ac4fad15b158cafe305d669fe54969e
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2025-01-17 13:44:09 +0100

    build(react): add lazy loading

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,27 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {
-  Login,
-  NotFound,
-  Home,
-  Playground,
-  Services,
-} from "@/pages";
+import { Suspense, lazy } from "react";
 import PrivateRoute from "./PrivateRoute";
 import AuthProvider from "@/contexts/AuthProvider";
-import Layout from "@/components/Layout";
 import { ReactFlowProvider } from "@xyflow/react";
 import { DnDProvider } from "@/contexts/DnDContext";
 
+const Login = lazy(() => import("@/pages/Login"));
+const NotFound = lazy(() => import("@/pages/NotFound"));
+const Home = lazy(() => import("@/pages/Home"));
+const Playground = lazy(() => import("@/pages/Playground"));
+const Services = lazy(() => import("@/pages/Services"));
+const Layout = lazy(() => import("@/components/Layout"));
+
 function Router() {
   return (
-      <BrowserRouter
-        future={{
-          v7_relativeSplatPath: true,
-          v7_startTransition: true,
-        }}
-      >
-        <AuthProvider>
-            <Routes>
+    <BrowserRouter
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true,
+      }}
+    >
+      <AuthProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
             <Route path="/login/*" element={<Login />} />
             <Route path="/" element={<Layout><PrivateRoute /></Layout>}>
               <Route index element={<Home />} />
@@ -31,18 +32,19 @@ function Router() {
             <Route path="/playground/:id" element={
               <ReactFlowProvider>
                 <DnDProvider>
-                <Layout>
-                  <PrivateRoute />
-                </Layout>
+                  <Layout>
+                    <PrivateRoute />
+                  </Layout>
                 </DnDProvider>
               </ReactFlowProvider>
-              }>
+            }>
               <Route index element={<Playground />} />
             </Route>
-            </Routes>
-        </AuthProvider>
+          </Routes>
+        </Suspense>
+      </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Router
+export default Router;
