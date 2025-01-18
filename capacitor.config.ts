40c2a9a038928be33384f0b3100ad374fa8d4b8f
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2025-01-18 20:26:25 +0100

    build(capacitor): attempt to fix the capacitor oauth2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,26 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'net.triggermenot.app',
   appName: 'TriggerMeNot',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    iosScheme: 'http',
+    hostname: 'localhost:8081',
+    cleartext: true
+  },
+  plugins: {
+    App: {
+      domain: 'localhost:8081',
+      paths: ['/login', '/terms', '/reset-password', '/services', '/settings', '/playground', '/playground/:id']
+    }
+  },
+  ios: {
+    contentInset: 'automatic',
+    preferredContentMode: "mobile"
+  },
+  android: {
+    backgroundColor: "#FFFFFF",
+  }
 };
 
 export default config;
