5e653e7f71387f2632e3da0bf62b883de296e684
neo-jgrec <jean-yanis.jeffroy@epitech.eu>
2025-01-19 01:04:28 +0100

    build(capacitor): edit config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,25 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'net.triggermenot.app',
   appName: 'TriggerMeNot',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    iosScheme: 'http',
+    hostname: 'localhost:8081',
+    cleartext: true
+  },
+  plugins: {
+    App: {
+      domain: 'localhost:8081',
+    }
+  },
+  ios: {
+    contentInset: 'automatic',
+    preferredContentMode: "mobile"
+  },
+  android: {
+    backgroundColor: "#FFFFFF",
+  }
 };
 
 export default config;
